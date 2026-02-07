e209f0ae1b5238d99d09a4a9f9b7014c8df6f952
celophi <csharris@protonmail.com>
2026-02-07 13:52:21 -0800

    Add PSY-Q / libcd / SYS (#15)
    - Adds some symbols for vendor functions.

---
+++ src/psyq/libcd/SYS.c
@@ -0,0 +1,43 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdStatus);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdMode);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdLastCom);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdLastPos);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdReset);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdFlush);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdSetDebug);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdComstr);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdIntstr);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdSync);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdReady);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdSyncCallback);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdReadyCallback);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdControl);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdControlF);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdControlB);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdMix);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdGetSector);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdGetSector2);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdDataCallback);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/SYS", CdDataSync);
