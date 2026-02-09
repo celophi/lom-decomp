f0faf20afdf881e22d800cff779c0a34cc2aefb3
celophi <csharris@protonmail.com>
2026-02-08 18:01:26 -0800

    Remove generated C files

--- src/main_u1.c
+++
@@ -1,7 +0,0 @@
-#include "common.h"
-
-INCLUDE_ASM("asm/nonmatchings/main_u1", func_80010000);
-
-INCLUDE_ASM("asm/nonmatchings/main_u1", func_80011358);
-
-INCLUDE_ASM("asm/nonmatchings/main_u1", func_80011638);
