9ea7a2280e3b0f7274e81f207a08ed95db95c4b0
celophi <csharris@protonmail.com>
2026-02-08 18:04:23 -0800

    Add cd pause clear state decomp (#19)

---
+++ src/psyq/libetc/INTR.c
@@ -0,0 +1,17 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libetc/INTR", ResetCallback);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libetc/INTR", InterruptCallback);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libetc/INTR", DMACallback);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libetc/INTR", VSyncCallback);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libetc/INTR", VSyncCallbacks);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libetc/INTR", StopCallback);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libetc/INTR", RestartCallback);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libetc/INTR", CheckCallback);
