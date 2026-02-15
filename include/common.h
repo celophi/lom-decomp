544312e3c7111e2748dc198bffe904b17faa83cc
celophi <csharris@protonmail.com>
2026-02-14 22:40:45 -0800

    Refactor CD_Initialize code (#33)

--- include/common.h
+++ include/common.h
@@ -3,19 +3,25 @@
 
 #include "include_asm.h"
 
-typedef unsigned char u_char;
-typedef unsigned short u_short;
-typedef unsigned int u_int;
-typedef unsigned long u_long;
-typedef unsigned char undefined;
-typedef unsigned char undefined1;
-typedef unsigned short undefined2;
-typedef unsigned int undefined4;
+typedef unsigned char   u_char;
+typedef unsigned short  u_short;
+typedef unsigned int    u_int;
+typedef unsigned long   u_long;
 
-typedef int s32;
-typedef unsigned int u32;
-typedef unsigned char u8;
-typedef signed char s8;
-typedef unsigned short u16;
+typedef unsigned char   undefined;
+typedef unsigned char   undefined1;
+typedef unsigned short  undefined2;
+typedef unsigned int    undefined4;
+
+typedef int             s32;
+typedef unsigned int    u32;
+typedef unsigned char   u8;
+typedef signed char     s8;
+typedef unsigned short  u16;
+
+// Boolean / null macros
+#define TRUE    1
+#define FALSE   0
+#define NULL    ((void*)0)
 
 #endif
