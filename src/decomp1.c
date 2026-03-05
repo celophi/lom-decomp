53b5c797e94e9ec4622e8a18d937c43d11e5e49f
celophi <csharris@protonmail.com>
2026-03-05 08:45:29 -0800

    correct some documentation

--- src/decomp1.c
+++ src/decomp1.c
@@ -1,7 +1,7 @@
 #include "decomp1.h"
 
 /**
- * decomp.me link (93.64%) https://decomp.me/scratch/mXgky
+ * decomp.me link (100%) https://decomp.me/scratch/mXgky
  */
 void FUN_8001160c(void) 
 {
@@ -9,7 +9,7 @@ void FUN_8001160c(void)
 }
 
 /**
- * decomp.me link (87.50%) https://decomp.me/scratch/Oy5Dh
+ * decomp.me link (98.25%) https://decomp.me/scratch/Oy5Dh
  */
 void FUN_80011638(s32 arg0)
 {
@@ -17,31 +17,30 @@ void FUN_80011638(s32 arg0)
     s32 *info;
     u8 *ptr;
     s32 temp;
-
+    
     if (arg0 == 0xFF)
     {
         return;
     }
-
+    
     base = (arg0 + 0x93) & 0xFFFF;
     FUN_800141ec(base, 0x80180000);
     CD_WaitForQueueEmpty();
 
-    info = (s32 *)0x80180004;
-    ptr  = &D_80046FE0;
-    base = 0x80180000;
+    info = (s32 *) 0x80180004;
+    ptr = &D_80046FE0; 
+    base = 0x80180000; 
+   
+    func_80016E7C((u_char *) ((*info) + base), ptr, info[1] - (*info)); 
+    func_80022AE8(info[1] + base, 1); 
     
-    func_80016E7C((u_char *)(*info + base), ptr, info[1] - *info);
-    func_80022AE8(info[1] + base, 1);
-
     temp = func_80022040(ptr);
-
     D_80042FB8 = temp;
     FUN_8002279c(temp, 0x7f);
 }
 
 /**
- * decomp.me link: (60.71%) https://decomp.me/scratch/DRBPP
+ * decomp.me link: (100%) https://decomp.me/scratch/DRBPP
  */
 void FUN_800116d8(int arg0, s16 arg1, s16 arg2, s16 arg3) 
 {
