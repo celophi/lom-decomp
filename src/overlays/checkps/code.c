2e7c3531f06092054f82fe8409d274ec74e05972
celophi <csharris@protonmail.com>
2026-03-15 09:44:44 -0700

    Added decompilation

---
+++ src/overlays/checkps/code.c
@@ -0,0 +1,16 @@
+#include "checkps.h"
+
+/**
+ * decomp.me link (100%) https://decomp.me/scratch/bzlSh
+ */
+s32 FUN_8004fd14(s32 arg0) 
+{
+    func_80050080();
+    func_8004FEE8(arg0);
+    
+    do {
+        func_8004FD68(arg0);
+    } while (D_8005D060 == 0);
+    
+    return 8;
+}
