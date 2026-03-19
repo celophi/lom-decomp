6826a1a752ba97972e455d7120f8dcd29468fa0b
celophi <csharris@protonmail.com>
2026-03-19 10:30:44 -0700

    Identify Psy-Q libs (#94)

---
+++ src/psyq/libc2/rand.c
@@ -0,0 +1,5 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libc2/rand", rand);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libc2/rand", srand);
---
+++ src/psyq/libcard/C171.c
@@ -0,0 +1,3 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcard/C171", _card_info);
