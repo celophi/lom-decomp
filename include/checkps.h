58e69b1ca657ce6b0705b6af7a4ffbc9c7bcedbc
celophi <csharris@protonmail.com>
2026-03-19 10:37:10 -0700

    Improve decompilation match for func_80050258 (#95)

--- include/checkps.h
+++ include/checkps.h
@@ -11,8 +11,8 @@ extern u32 D_80052428;
 extern s32 D_80042FB0;
 extern s32 D_80061088;
 extern u8 D_8005D088;
-extern u32 D_8005D068;
-extern u32 D_8005D078;
+extern s32 D_8005D068[4];
+extern s32 D_8005D078[3];
 
 void func_80050080(void);
 void func_8004FEE8(int param_1);
