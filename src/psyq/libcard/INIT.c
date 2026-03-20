840ecc25f8c6b2ccb8fe2ab51e7492d22bd4894c
celophi <csharris@protonmail.com>
2026-03-19 18:09:48 -0700

    Added StopCARD

--- src/psyq/libcard/INIT.c
+++ src/psyq/libcard/INIT.c
@@ -3,3 +3,5 @@
 INCLUDE_ASM("asm/nonmatchings/psyq/libcard/INIT", InitCARD);
 
 INCLUDE_ASM("asm/nonmatchings/psyq/libcard/INIT", StartCARD);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcard/INIT", StopCARD);
