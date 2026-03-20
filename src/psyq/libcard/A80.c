d386982cc609dad317556b38f1e2b309925d1fff
celophi <csharris@protonmail.com>
2026-03-19 20:51:21 -0700

    Identify additional Psy-Q libraries (#96)

---
+++ src/psyq/libcard/A80.c
@@ -0,0 +1,3 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcard/A80", _new_card);
---
+++ src/psyq/libcard/INIT.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcard/INIT", InitCARD);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcard/INIT", StartCARD);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcard/INIT", StopCARD);
