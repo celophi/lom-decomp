f404244c2585f6e64624315851d73c3f7010a8aa
celophi <csharris@protonmail.com>
2026-03-21 10:52:55 -0700

    Added PRESS functions

--- Makefile
+++ Makefile
@@ -139,6 +139,7 @@ SRCS_G0 := \
 	src/psyq/libcard/END.c \
 	src/psyq/libcard/FORMAT.c \
 	src/psyq/libcard/A92.c \
+	src/psyq/libpress/PRESS.c \
 	src/decompression.c \
 	src/unk1.c \
 	src/unk2.c \
---
+++ src/psyq/libpress/PRESS.c
@@ -0,0 +1,33 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libpress/PRESS", DecDCTReset);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libpress/PRESS", DecDCTGetEnv);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libpress/PRESS", DecDCTPutEnv);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libpress/PRESS", DecDCTin);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libpress/PRESS", DecDCTout);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libpress/PRESS", DecDCTinSync);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libpress/PRESS", DecDCToutSync);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libpress/PRESS", DecDCTinCallback);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libpress/PRESS", DecDCToutCallback);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libpress/PRESS", MDEC_reset);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libpress/PRESS", MDEC_in);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libpress/PRESS", MDEC_out);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libpress/PRESS", MDEC_in_sync);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libpress/PRESS", MDEC_out_sync);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libpress/PRESS", MDEC_register);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libpress/PRESS", MDEC_timeout);
