d36dad96cb0ebe865430c9c1558de7fc92daa0b4
celophi <csharris@protonmail.com>
2026-03-21 12:29:26 -0700

    Identify more SDK libs (#99)

---
+++ src/psyq/libc2/PUTCHAR.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libc2/PUTCHAR", _putchar);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libc2/PUTCHAR", _putchar_flash);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libc2/PUTCHAR", putchar);
