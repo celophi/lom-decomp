78c0c5b7b2339d3e3bbd6ba0d25ec1165bae9a00
celophi <csharris@protonmail.com>
2026-03-22 10:03:39 -0700

    Add MatrixNormal

--- src/psyq/libgte/MSC02.c
+++ src/psyq/libgte/MSC02.c
@@ -5,3 +5,5 @@ INCLUDE_ASM("asm/nonmatchings/psyq/libgte/MSC02", InvSquareRoot);
 INCLUDE_ASM("asm/nonmatchings/psyq/libgte/MSC02", VectorNormalS);
 
 INCLUDE_ASM("asm/nonmatchings/psyq/libgte/MSC02", MSC02_OBJ_100);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libgte/MSC02", MatrixNormal);
