5beb734b5b339b3650410cf410acbf644417d6fb
celophi <csharris@protonmail.com>
2026-03-22 10:01:32 -0700

    Added internal function

--- src/psyq/libgte/MSC02.c
+++ src/psyq/libgte/MSC02.c
@@ -3,3 +3,5 @@
 INCLUDE_ASM("asm/nonmatchings/psyq/libgte/MSC02", InvSquareRoot);
 
 INCLUDE_ASM("asm/nonmatchings/psyq/libgte/MSC02", VectorNormalS);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libgte/MSC02", MSC02_OBJ_100);
