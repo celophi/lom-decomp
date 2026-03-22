d0b08916e13437a910f9bfd3bc1a3cf89541c19e
celophi <csharris@protonmail.com>
2026-03-22 09:19:59 -0700

    Added csin

--- src/psyq/libgte/COR_03.c
+++ src/psyq/libgte/COR_03.c
@@ -1,3 +1,7 @@
 #include "common.h"
 
 INCLUDE_ASM("asm/nonmatchings/psyq/libgte/COR_03", csin_1);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libgte/COR_03", csin);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libgte/COR_03", func_8001CBEC);
