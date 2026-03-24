eb975bb1b408f1f8c5c069254c2fe19cce678edb
celophi <csharris@protonmail.com>
2026-03-23 17:49:26 -0700

    Fix COR_03

--- src/psyq/libcd/EVENT.c
+++ src/psyq/libcd/EVENT.c
@@ -5,3 +5,7 @@ INCLUDE_ASM("asm/nonmatchings/psyq/libcd/EVENT", CdInit);
 INCLUDE_ASM("asm/nonmatchings/psyq/libcd/EVENT", EVENT_OBJ_90);
 
 INCLUDE_ASM("asm/nonmatchings/psyq/libcd/EVENT", EVENT_OBJ_CC);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/EVENT", EVENT_OBJ_F4);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/EVENT", EVENT_OBJ_11C);
--- src/psyq/libgte/COR_03.c
+++ src/psyq/libgte/COR_03.c
@@ -4,4 +4,4 @@ INCLUDE_ASM("asm/nonmatchings/psyq/libgte/COR_03", csin_1);
 
 INCLUDE_ASM("asm/nonmatchings/psyq/libgte/COR_03", csin);
 
-INCLUDE_ASM("asm/nonmatchings/psyq/libgte/COR_03", func_8001CBEC);
+INCLUDE_ASM("asm/nonmatchings/psyq/libgte/COR_03", MSC00_OBJ_0);
