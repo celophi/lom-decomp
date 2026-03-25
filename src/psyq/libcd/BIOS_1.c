7e068fcf20f51d84677e966936487ca77766263e
celophi <csharris@protonmail.com>
2026-03-25 10:34:45 -0700

    Add BIOS functions from libcd (#102)

---
+++ src/psyq/libcd/BIOS_1.c
@@ -0,0 +1,21 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/BIOS_1", BIOS_OBJ_0);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/BIOS_1", CD_sync);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/BIOS_1", CD_ready);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/BIOS_1", CD_cw);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/BIOS_1", CD_vol);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/BIOS_1", CD_flush);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/BIOS_1", CD_initvol);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/BIOS_1", CD_initintr);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/BIOS_1", CD_init);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libcd/BIOS_1", CD_datasync);
