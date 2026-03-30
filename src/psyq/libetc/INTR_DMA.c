61eab3d3f93f3c41c01ba6ddfff08baba2eb8001
celophi <csharris@protonmail.com>
2026-03-29 17:24:08 -0700

    Identify Psy-Q libs (#105)

---
+++ src/psyq/libetc/INTR_DMA.c
@@ -0,0 +1,9 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libetc/INTR_DMA", startIntrDMA);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libetc/INTR_DMA", INTR_DMA_OBJ_4C);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libetc/INTR_DMA", INTR_DMA_OBJ_1CC);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libetc/INTR_DMA", INTR_DMA_OBJ_278);
---
+++ src/psyq/libspu/SPU.c
@@ -0,0 +1,29 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libspu/SPU", _spu_init);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libspu/SPU", SPU_OBJ_280);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libspu/SPU", _spu_FiDMA);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libspu/SPU", _spu_Fr_);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libspu/SPU", _spu_t);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libspu/SPU", _spu_Fw);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libspu/SPU", _spu_Fr);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libspu/SPU", _spu_FsetRXX);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libspu/SPU", _spu_FsetRXXa);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libspu/SPU", _spu_FgetRXXa);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libspu/SPU", _spu_FsetPCR);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libspu/SPU", SPU_OBJ_A88);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libspu/SPU", SPU_OBJ_AB0);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libspu/SPU", _spu_Fw1ts);
