afd9c01e93d734270fe431b9f7594a44f98701ec
celophi <csharris@protonmail.com>
2026-04-11 20:10:21 -0700

    Add initial decompilation

--- include/decomp2.h
+++ include/decomp2.h
@@ -5,6 +5,7 @@
 
 extern u32 D_80123FD8;
 extern u32 D_801B32D8;
+extern u32 D_801026D0;
 
 u32* FUN_80015c28(void);
 u32* FUN_80015c38(void);
--- src/decomp2.c
+++ src/decomp2.c
@@ -15,3 +15,11 @@ u32* FUN_80015c38(void)
 {
     return &D_801B32D8;
 }
+
+/**
+ * decomp.me link (100%) https://decomp.me/scratch/fl1lB
+ */
+u32* func_80015C48(void)
+{
+    return &D_801026D0;
+}
