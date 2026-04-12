76acb6a29db968b268e66ea34b2d26251b093891
celophi <csharris@protonmail.com>
2026-04-11 17:30:16 -0700

    Add initial decomp

---
+++ include/decomp2.h
@@ -0,0 +1,10 @@
+#ifndef _DECOMP1_H
+#define _DECOMP1_H
+
+#include "common.h"
+
+extern u32 D_80123FD8;
+
+u32* FUN_80015c28(void);
+
+#endif
---
+++ src/decomp2.c
@@ -0,0 +1,9 @@
+#include "decomp2.h"
+
+/**
+ * 
+ */
+u32* FUN_80015c28(void)
+{
+    return &D_80123FD8;
+}
