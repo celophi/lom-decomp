e2c5fbcd077222b709fa6d6f7e0b9c68b5852d1e
celophi <csharris@protonmail.com>
2026-04-12 17:12:22 -0700

    Fix match

--- src/overlays/checkps/data.c
+++ src/overlays/checkps/data.c
@@ -4,8 +4,8 @@
  * Lookup table for test pattern vertex offsets,
  * used in DrawSymmetricTestPattern. Contains 4 pairs of X/Y sign values.
  */
-const u8 g_testPatternVertexTable[8] = {
-    0x05, 0x01, 0xFF, 0x01, 0x01, 0xFF, 0xFF, 0xFF,
+const s8 g_testPatternVertexTable[8] = {
+    1, 1, -1, 1, 1, -1, -1, -1,
 };
 
 /**
@@ -13,5 +13,5 @@ const u8 g_testPatternVertexTable[8] = {
  * since the vertex table is only 8 bytes but the next item starts at an offset of 20 bytes.
  */
 const u8 g_testPatternVertexTablePadding[12] = {
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 };
