07be901422bfb4691ec92f30c90b9e0791075939
celophi <csharris@protonmail.com>
2026-04-17 20:25:10 -0700

    Add additional unk7 decompilations (#157)

--- src/decomp3.c
+++ src/decomp3.c
@@ -136,3 +136,174 @@ s32 func_800221BC(s32 arg0, s32 arg1, s32 arg2, s32 arg3)
     }
     return var_v0;
 }
+
+/**
+ * decomp.me link (100%) https://decomp.me/scratch/lu9nS
+ */
+void func_80022240(s32 arg0, s32 arg1)
+{
+    s32 temp_a1;
+
+    D_8004D430[0] = arg0;
+    temp_a1 = arg1 & 0xFFFFFF;
+    D_8004D430[1] = temp_a1;
+    func_80028E84(0x21);
+}
+
+/**
+ * decomp.me link (100%) https://decomp.me/scratch/0mLzI
+ */
+void func_8002227C(s32 arg0)
+{
+    D_8004D430[0] = arg0 & 0x3FF;
+    func_80028E84(0x30);
+}
+
+/**
+ * decomp.me link (100%) https://decomp.me/scratch/yZloM
+ */
+s32 func_800222A8(void)
+{
+    s32 bits;
+    unsigned char* ptr;
+    s32 new_var;
+    s32 acc;
+    unsigned int mask;
+    new_var = D_8004D400;
+    bits = new_var;
+    acc = bits == 0;
+    if (acc)
+    {
+        return 0;
+    }
+    ptr = D_8004B430;
+    acc = 0;
+    mask = 0x1000;
+    do
+    {
+        if (bits & mask)
+        {
+            acc |= *((s32*)(ptr + 0x28));
+        }
+        mask <<= 1;
+        ptr += 0x118;
+    } while (mask & 0xFFFFFF);
+    return acc & 0xFFFFFF;
+}
+
+/**
+ * decomp.me link (100%) https://decomp.me/scratch/OvqYq
+ */
+s32 func_80022310(s32 arg0)
+{
+    s32 new_var;
+    s32 bits;
+    unsigned char* ptr;
+    unsigned int mask;
+    if (arg0 == 0)
+    {
+        return 0;
+    }
+    bits = D_8004D400;
+    if (bits == 0)
+    {
+        return 0;
+    }
+    ptr = D_8004B430;
+    mask = 0x1000;
+    do
+    {
+        if ((new_var = bits) & mask)
+        {
+            if (arg0 == (*((s32*)(ptr + 0x28))))
+            {
+                return 1;
+            }
+        }
+        mask <<= 1;
+        ptr += 0x118;
+    } while (mask & 0xFFFFFF);
+    return 0;
+}
+
+/**
+ * decomp.me link (100%) https://decomp.me/scratch/9qTjH
+ */
+void func_8002237C(s32 arg0)
+{
+    int* new_var2;
+    int new_var;
+    new_var = (arg0 == 1) ? (0x81) : (0x80);
+    new_var2 = &new_var;
+    func_80028E84(new_var);
+}
+
+/**
+ * decomp.me link (100%) https://decomp.me/scratch/x94md
+ */
+void func_800223B0(s32 arg0)
+{
+    D_8004D430[0] = arg0;
+    func_80028E84(0x90);
+}
+
+/**
+ * decomp.me link (100%) https://decomp.me/scratch/y9TAf
+ */
+void func_800223D8(s32 arg0)
+{
+    D_8004D430[0] = arg0;
+    func_80028E84(0x92);
+}
+
+/**
+ * decomp.me link (100%) https://decomp.me/scratch/qqSuG
+ */
+void FUN_80022400(u32 param_1)
+{
+    s32 var_a0;
+
+    switch (param_1)
+    {
+    case 1:
+        var_a0 = 0x9B;
+        break;
+    case 2:
+        var_a0 = 0x9D;
+        break;
+    case 3:
+        var_a0 = 0x9F;
+        break;
+    default:
+        var_a0 = 0x99;
+        break;
+    }
+
+    func_80028E84(var_a0);
+}
+
+/**
+ * decomp.me link (100%) https://decomp.me/scratch/iREFc
+ */
+void func_8002246C(u32 arg0)
+{
+    s32 var_a0;
+
+    switch (arg0)
+    {
+    case 1:
+        var_a0 = 0x9A;
+        break;
+    case 2:
+        var_a0 = 0x9C;
+        break;
+    case 3:
+        var_a0 = 0x9E;
+        break;
+    default:
+        var_a0 = 0x98;
+        break;
+    }
+
+    func_80028E84(var_a0);
+}
