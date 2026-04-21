47d4460ab1c24db60c2dc32e65d63a632f407e3e
celophi <csharris@protonmail.com>
2026-04-21 15:33:52 -0700

    Add decomp for func_80023830 (#186)

--- include/decomp5.h
+++ include/decomp5.h
@@ -21,8 +21,39 @@ typedef struct
 
 extern s32 D_8003EC4C;
 extern u8 D_8004C340[];
+extern u8 D_8004B430[];
+extern s32 D_8003EC7C;
+extern s32 D_8003EC6C;
+extern s32 D_8003EC44;
+extern u8 D_8004F760[];
+extern s16 D_8003EC64;
+extern s32 D_8003EC78;
+extern s16 D_8003EC42;
+extern s32 D_8003EC74;
+extern s16 D_8003EC40;
+extern s32 D_8003EC70;
+extern s32 D_8003EC68;
+extern s32 D_8003EC24;
+extern s32 D_8003EC28;
+extern void *D_8003EC5C;
+extern void *D_8003EC58;
+extern u8 D_8004C2D0[];
+extern u8 D_8004D400[];
+extern u8 D_8004F830[];
+extern u8 D_8004F750[];
+extern u8 D_8004D388[];
+extern u8 D_8003EC30[];
+extern u8 D_80049130[];
+extern u8 D_8004C260[];
 
 extern s32 func_80024230(s32);
 extern s32 func_800235F8(void);
+extern void func_80028E34(int, volatile short, void *, int);
+extern void func_80023EF0(int);
+
+inline static u8* off(u8* p, int o)
+{
+    return p + o;
+}
 
 #endif
