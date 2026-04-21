36f39bfbbf805fa4e67c7c814ae3c2636cba8323
celophi <csharris@protonmail.com>
2026-04-21 11:49:16 -0700

    Add initial decomp

--- include/decomp5.h
+++ include/decomp5.h
@@ -11,7 +11,16 @@ typedef struct
     s32 unk18;
 } UnknownStruct;
 
+typedef struct
+{
+    char _pad0[0x14];
+    u32 unk14;
+    char _pad1[0x4];
+    u32 unk1C;
+} SomeStruct;
+
 extern s32 D_8003EC4C;
+extern u8 D_8004C340[];
 
 extern s32 func_80024230(s32);
 extern s32 func_800235F8(void);
