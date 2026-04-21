c1b23895ed522c6de336dc6a5eb682d8f85ff50a
celophi <csharris@protonmail.com>
2026-04-21 11:55:35 -0700

    Add initial decomp for func_8002376C (#185)

--- src/decomp5.c
+++ src/decomp5.c
@@ -92,3 +92,42 @@ s32 func_8002371C(void* arg0, s32 arg1)
     }
     return ret;
 }
+
+/**
+ * decomp.me link (99.90%) https://decomp.me/scratch/Awfhy
+ */
+s32 func_8002376C(void* arg0, s32 arg1, s32 arg2, s32 arg3)
+{
+    s32 new_var;
+    s32 var_v0;
+    SomeStruct* s;
+    u8* base;
+
+    s32 ret_val;
+    func_800236EC();
+    var_v0 = -1;
+    if (func_800235F8(arg0) == 0)
+    {
+        new_var = arg0;
+        s = (SomeStruct*)arg0;
+
+        SpuSetTransferStartAddr(arg3);
+        base = (u8*)arg0;
+        base = base + 0x40;
+        func_80023660((s32)(base + (s->unk1C * 0x10)), s->unk14);
+        func_800235A8((s32*)base, (s32*)(D_8004C340 + (arg2 * 0x10)), arg3, s->unk1C);
+        var_v0 = 0;
+        if (arg1 != 0)
+        {
+            func_800236EC();
+        }
+        ret_val = 0;
+    }
+    else
+    {
+        D_8003EC4C = -1;
+        ret_val = -1;
+    }
+    new_var = ret_val;
+    return new_var;
+}
