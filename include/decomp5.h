c6f46f9d0b4b6559aa9ad1ebfb5d2149e5ddfc05
celophi <csharris@protonmail.com>
2026-04-20 20:33:26 -0700

    Add func_8002371C decomp (#183)

--- include/decomp5.h
+++ include/decomp5.h
@@ -3,8 +3,17 @@
 
 #include "common.h"
 
+typedef struct
+{
+    char pad0[0x10];
+    s32 unk10;
+    char pad1[0x4];
+    s32 unk18;
+} UnknownStruct;
+
 extern s32 D_8003EC4C;
 
-extern s32 func_80024230(s32); 
+extern s32 func_80024230(s32);
+extern s32 func_800235F8(void);
 
 #endif
