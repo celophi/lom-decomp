6a5f7bde9b4c652de8f1e3269692aa9f82316ba7
celophi <csharris@protonmail.com>
2026-04-21 20:37:40 -0700

    Add decomp for 80014ADC (#190)

---
+++ include/decomp6.h
@@ -0,0 +1,13 @@
+#ifndef _DECOMP6_H
+#define _DECOMP6_H
+
+#include "common.h"
+
+extern s32 D_801ED7A4;
+
+extern void func_80015674(void);
+extern void func_80015708(void *);
+extern void func_8002E958(int, int, int, int);
+extern void func_80030DF8(void *, void *);
+
+#endif
