5ee676a821bd90b2ff3ee7912a3f39a624c1fd35
celophi <csharris@protonmail.com>
2026-04-21 18:54:40 -0700

    Add additional Psy-Q libs (#188)

---
+++ src/psyq/libapi/COUNTER.c
@@ -0,0 +1,11 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libapi/COUNTER", SetRCnt);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libapi/COUNTER", GetRCnt);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libapi/COUNTER", StartRCnt);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libapi/COUNTER", StopRCnt);
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libapi/COUNTER", ResetRCnt);
---
+++ src/psyq/libspu/S_R.c
@@ -0,0 +1,3 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/psyq/libspu/S_R", SpuRead);
