1fcd3bef91dce4f0737e3c154cf2f403069afc44
celophi <csharris@protonmail.com>
2026-04-21 17:28:57 -0700

    Add decompilations for unk8 in SLUS (#187)

--- src/decomp5.c
+++ src/decomp5.c
@@ -272,3 +272,81 @@ void func_80023830(void)
     func_80028E34(4, 0x03FFF000, a2, a3);
     func_80023EF0(1);
 }
+
+/**
+ * decomp.me link (100%) https://decomp.me/scratch/0YmTg
+ */
+void func_80023AD0(void)
+{
+    s32 temp_v0;
+
+    SpuStart();
+    func_80023E90(4, &D_8004D360);
+    func_80024200(0);
+    SpuSetTransferStartAddr(0x1010);
+    func_80023660(&D_8003D170, 0x40);
+    func_800236EC();
+    func_80023830();
+    SpuSetIRQ(0);
+    func_800240D0(0);
+
+    do
+    {
+        /* wait for condition */
+    } while (func_80023CA0(0xF2000002, 0x44E8, 0x1000) == 0);
+
+    do
+    {
+        /* wait for condition */
+    } while (func_80023D74(0xF2000002) == 0);
+
+    do
+    {
+        temp_v0 = func_800167AC(0xF2000002, 2, 0x1000, func_8002A134);
+        D_8003EC14 = temp_v0;
+    } while (temp_v0 == -1);
+
+    do
+    {
+        /* wait for completion */
+    } while (func_800167DC(D_8003EC14) == 0);
+}
+
+/**
+ * decomp.me link (100%) https://decomp.me/scratch/z36q3
+ */
+void func_80023BB8(s32 arg0)
+{
+    D_8003EC48 = arg0;
+    arg0 += 0x600;
+    D_8003EC50 = arg0;
+    arg0 += 0x300;
+    D_8003EC54 = arg0;
+}
+
+/**
+ * decomp.me link (100%) https://decomp.me/scratch/VenON
+ */
+void func_80023BE0(void)
+{
+    if (D_8003EC4C == 1)
+    {
+        func_80023660(&D_8003D170, 0x40);
+        func_800236EC();
+    }
+    do
+    {
+
+    } while (func_80023DA4(0xF2000002) == 0);
+    func_80023C90(0xF2000002, 2);
+    do
+    {
+
+    } while (func_80023C80(D_8003EC14) == 0);
+    do
+    {
+
+    } while (func_800167BC(D_8003EC14) == 0);
+    func_8002427C(0xFFFFFF);
+    func_80023E10();
+}
