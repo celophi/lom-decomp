4e2973ae0d3fd8f958872d11df86dd6743dba39b
celophi <csharris@protonmail.com>
2026-04-22 22:46:35 -0700

    Add decomp func_800161DC (#194)

--- src/decomp8.c
+++ src/decomp8.c
@@ -1,5 +1,8 @@
 #include "decomp8.h"
 
+/**
+ * decomp.me link (100%) https://decomp.me/scratch/mLcZm
+ */
 void func_8001615C(u8* arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4)
 {
     u8* p;
@@ -21,4 +24,19 @@ void func_8001615C(u8* arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4)
             func_800165CC(ch, a3_val, arg4);
         } while ((s32)p < (s32)end);
     }
+}
+
+/**
+ * decomp.me link (99.50%) https://decomp.me/scratch/ENN60
+ */
+void func_800161DC(s32 arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4)
+{
+    s32 q;
+    int new_var;
+    D_80047404 = arg1;
+    D_80047408 = arg2;
+    q = arg0 / 10;
+    new_var = q * 10;
+    func_800165CC(q + 0x30, arg3, arg4);
+    func_800165CC((arg0 - new_var) + 0x30, arg3, arg4);
 }
