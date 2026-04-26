3be14d4e9abd34526b5873a7c98bcbf75ea58958
celophi <csharris@protonmail.com>
2026-04-25 21:13:59 -0700

    Add decompilation for func_80140358 (#200)

--- include/movie.h
+++ include/movie.h
@@ -26,7 +26,61 @@ typedef struct
     u_char unk9f;
 } SRC_801ED500;
 
+typedef struct
+{
+    u8* unk0;
+    u8* unk4;
+    u8* unk8;
+    u8* unkC;
+    u8* unk10;
+    u8* unk14;
+    u8* unk18;
+    u8* unk1C;
+    RECT rects[3];
+    u32 unk38;
+    u32 unk3C;
+    u8 pad_40[4];
+    u32 unk44;
+    u32 unk48;
+    u32 unk4C;
+    u32 unk50;
+    u32 unk54;
+    u32 unk58[9];
+    u8 pad_7C[2];
+    u16 unk7E;
+    u32 unk80;
+    u32 unk84;
+    u32 unk88;
+    u32 unk8C;
+    s8 unk90;
+    s8 unk91;
+    s8 unk92;
+    s8 unk93;
+    s8 unk94;
+    s8 unk95;
+    s8 unk96;
+    s8 unk97;
+    s8 unk98;
+    s8 unk99;
+    s8 unk9A;
+    s8 unk9B;
+    s8 unk9C;
+    s8 unk9D;
+    s8 unk9E;
+    s8 unk9F;
+} UnkState;
+typedef struct
+{
+    u8 pad[0x38];
+    u32 unk38;
+} AllocInfo;
+
 extern u_char g_cdAudioReady;
+extern void *D_80180014;
+extern u8 D_801ED590;
+extern void func_80140AC0(void);
+extern void func_801416C4(void);
+extern u32 *func_80140F04(s32 param_1, u32 param_2);
 
 extern void CD_UpdateAndProcessQueue(void);
 extern s32 CD_GetErrorStatus(void);
