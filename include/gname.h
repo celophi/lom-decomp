52f89bfffc1a1f0c78813bb73328c84d73b4b382
celophi <csharris@protonmail.com>
2026-05-05 08:37:20 -0700

    Add decomp

---
+++ include/gname.h
@@ -0,0 +1,9 @@
+#ifndef _GNAME_H
+#define _GNAME_H
+
+#include "common.h"
+
+extern s32 D_8014F818[];
+extern s32 D_8014F828[];
+
+#endif
---
+++ src/overlays/gname/gname.c
@@ -0,0 +1,15 @@
+#include "gname.h"
+
+/**
+ * decomp.me (100%) https://decomp.me/scratch/ld2aW
+ */
+void func_801403E0(void)
+{
+    D_8014F828[0] = 0;
+    D_8014F828[1] = 0;
+    D_8014F828[2] = 0;
+    D_8014F818[0] = 0;
+    D_8014F818[1] = 0;
+    D_8014F818[2] = 0;
+    D_8014F818[3] = 0;
+}
