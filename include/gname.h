66a9e77b1c4d3a422e1fd704b191eb9d2f86a28d
celophi <csharris@protonmail.com>
2026-05-07 15:31:48 -0700

    Add initial decomp GNAME func_80141928 (#274)

--- include/gname.h
+++ include/gname.h
@@ -64,6 +64,7 @@ extern void *D_8014F84C;
 extern s32 D_8014F838;
 extern u8 D_80142EF4[];
 extern unsigned char D_80142CD4[];
+extern s32 D_80142E14;
 
 extern void func_800A3938(int, int);
 extern void func_8014139C(void);
