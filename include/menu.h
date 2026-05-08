ff81be5661bdb094702d28c74d46972d885f9446
celophi <csharris@protonmail.com>
2026-05-08 16:13:56 -0700

    Add decomp for MENU func_80140A48 (#289)

--- include/menu.h
+++ include/menu.h
@@ -14,6 +14,15 @@ extern s32 D_801690AC;
 extern s32 D_801690E8;
 extern s32 D_801690F4;
 extern s32 D_80169120;
+extern s32 D_800F22AC;
+extern s32 D_80122988;
+extern s32 D_8016955C;
+extern s32 D_801228C8;
+extern u8 D_80168778[];
+extern s32 D_80122730;
+extern s32 D_80169100;
+extern void *D_8012271C;
+extern s32 D_801229FC;
 extern u8 D_800FE778[];
 
 #endif
