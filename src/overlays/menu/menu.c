b222c7c8121530c6eec16779dfdd3f455e9a355a
celophi <csharris@protonmail.com>
2026-05-08 15:20:48 -0700

    Add decompilation MENU func_80140908 (#287)

---
+++ src/overlays/menu/menu.c
@@ -0,0 +1,16 @@
+#include "menu.h"
+
+void func_80140908(void)
+{
+    volatile u8 padding;
+    func_801410B0();
+    func_801410E8();
+    func_80141324();
+    D_801690F4 = -1;
+    func_800AA02C();
+    D_801690E8 = 0;
+    func_80140968();
+    D_801690AC = 0;
+    D_80169120 = 0;
+    func_801423D8();
+}
