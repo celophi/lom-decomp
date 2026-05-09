1721f93d6dbe09369ff2966e37e7e717c7315731
celophi <csharris@protonmail.com>
2026-05-08 21:15:11 -0700

    Add decomp MENU func_801410FC (#293)

--- include/menu.h
+++ include/menu.h
@@ -21,8 +21,18 @@ extern s32 D_801228C8;
 extern u8 D_80168778[];
 extern s32 D_80122730;
 extern s32 D_80169100;
-extern void *D_8012271C;
+extern void* D_8012271C;
 extern s32 D_801229FC;
 extern u8 D_800FE778[];
+extern u8 D_80160260[];
+extern s32 D_8016910C;
+
+typedef struct
+{
+    u16 unk0;
+    u16 unk2;
+    u16 unk4;
+    u16 unk6;
+} ArgStruct;
 
 #endif
