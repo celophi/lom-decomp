be2ce657b9f5b30c69cd24d6a3c7e47bda38e56e
celophi <csharris@protonmail.com>
2026-05-10 23:28:27 -0700

    Add decomp for func_80029A8C (#315)

--- include/decomp4.h
+++ include/decomp4.h
@@ -4,5 +4,26 @@
 #include "common.h"
 
 extern s16 D_8003EC6A;
+extern s32 D_8003EC60;
+extern s32 D_8003EC70;
+extern short D_8003EC64;
+extern s32 D_8003EC68;
+extern s32 D_8004F754;
+extern s16 D_8003D47C;
+extern s32 D_8004F7A0;
+extern s16 D_8003EC42;
+extern s32 D_8003EC3C;
+extern s32 D_8003EC78;
+extern s16 D_8003EC40;
+extern s32 D_8003EC38;
+extern s32 D_8003EC74;
+extern u8 D_80049130[];
+extern void *D_8003EC28;
+extern s32 D_8003EC24;
+extern s32 D_8004D400;
+
+extern void *g_akao_seq_channel0;
+extern u8 g_akao_xa_tracker[];
+extern u8 g_sfx_channels[];
 
 #endif
