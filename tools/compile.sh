e3e47c16f8a1fe08ff65a47fbcac4d776dc055ca
celophi <csharris@protonmail.com>
2026-05-14 08:56:10 -0700

    Improve main match (#327)

--- tools/compile.sh
+++ tools/compile.sh
@@ -8,7 +8,7 @@ TMPASM=$(mktemp --suffix=.s)
 cd /staging
 
 # Stage 1: Compile to assembly
-PATH=/opt/psx-gcc:$PATH /opt/psx-gcc/gcc -O2 -G0 -g -fsigned-char -Iinclude -S "$INPUT" -o "$TMPASM"
+/opt/psx-gcc/gcc -B/opt/psx-gcc/ -O2 -G0 -g -fsigned-char -Iinclude -S "$INPUT" -o "$TMPASM"
 
 # Stage 2: Assemble with maspsx pipeline (like your build does)
 cat "$TMPASM" | python3 tools/maspsx/maspsx.py --run-assembler -Iinclude -no-pad-sections --aspsx-version=2.77 -o "$OUTPUT"
