2168fdd19e88ff97522cfe59784bb221484a6f02
celophi <csharris@protonmail.com>
2026-05-21 12:55:40 -0700

    Combine structures decomp4 2 (#365)

--- include/decomp4.h
+++ include/decomp4.h
@@ -46,27 +46,76 @@ typedef struct AkaoChannelState
     u32 unk14;       /* 0x14 */
     u32 unk18;       /* 0x18 */
     u32 unk1C;       /* 0x1C */
-    u32 unk20;       /* 0x20 (low 16 bits + high 16 bits = unk22) */
+    u32 unk20;       /* 0x20 */
     u32 unk24;       /* 0x24 */
     u32 unk28;       /* 0x28 */
-    u8 _pad2C[8];    /* 0x2C - 0x33 */
+    s32 unk2C;       /* 0x2C */
+    s32 unk30;       /* 0x30 */
     u8* unk34;       /* 0x34 */
     u8 _pad38[8];    /* 0x38 - 0x3F */
     u32 unk40;       /* 0x40 */
     u8 _pad44[4];    /* 0x44 - 0x47 */
     u32 unk48;       /* 0x48 */
     u32 unk4C;       /* 0x4C */
-    u8 _pad50[0x0A]; /* 0x50 – 0x59 */
+    s32 unk50;       /* 0x50 */
+    u8 _pad54[6];    /* 0x54 - 0x59 */
     s16 unk5A;       /* 0x5A */
     u16 unk5C;       /* 0x5C */
     u16 unk5E;       /* 0x5E */
-    u32 unk60; /* 0x60 – 0x63 */
+    u32 unk60;       /* 0x60 */
     u16 unk64;       /* 0x64 */
     u16 unk66;       /* 0x66 */
     u16 unk68;       /* 0x68 */
     u16 unk6A;       /* 0x6A */
     u16 unk6C;       /* 0x6C */
-    u8 _pad6E[0xAA]; /* 0x6E – 0x117 */
+    u8 _pad6E[4];    /* 0x6E - 0x71 */
+    u16 unk72;       /* 0x72 */
+    u16 unk74[11];   /* 0x74 - 0x89 */
+    u16 unk8A;       /* 0x8A */
+    u16 unk8C;       /* 0x8C */
+    u8 _pad8E[6];    /* 0x8E - 0x93 */
+    u16 unk94;       /* 0x94 */
+    u16 unk96;       /* 0x96 */
+    u16 unk98;       /* 0x98 */
+    u16 unk9A;       /* 0x9A */
+    u16 unk9C;       /* 0x9C */
+    u16 unk9E;       /* 0x9E */
+    u8 _padA0[2];    /* 0xA0 - 0xA1 */
+    u16 unkA2;       /* 0xA2 */
+    u16 unkA4;       /* 0xA4 */
+    u8 _padA6[2];    /* 0xA6 - 0xA7 */
+    u16 unkA8;       /* 0xA8 */
+    u16 unkAA;       /* 0xAA */
+    s16 unkAC;       /* 0xAC */
+    u16 unkAE;       /* 0xAE */
+    u8 _padB0[6];    /* 0xB0 - 0xB5 */
+    u16 unkB6;       /* 0xB6 */
+    u16 unkB8;       /* 0xB8 */
+    u8 _padBA[2];    /* 0xBA - 0xBB */
+    u16 unkBC;       /* 0xBC */
+    u16 unkBE;       /* 0xBE */
+    u8 _padC0[24];   /* 0xC0 - 0xD7 */
+    u16 unkD8;       /* 0xD8 */
+    u16 unkDA;       /* 0xDA */
+    u16 unkDC;       /* 0xDC */
+    u16 unkDE;       /* 0xDE */
+    u8 _padE0[10];   /* 0xE0 - 0xE9 */
+    u16 unkEA;       /* 0xEA */
+    s16 unkEC;       /* 0xEC */
+    u16 unkEE;       /* 0xEE */
+    u16 unkF0;       /* 0xF0 */
+    s16 unkF2;       /* 0xF2 */
+    u16 unkF4;       /* 0xF4 */
+    u16 unkF6;       /* 0xF6 */
+    u8 _padF8[4];    /* 0xF8 - 0xFB */
+    u32 unkFC;       /* 0xFC */
+    s32 unk100;      /* 0x100 */
+    s32 unk104;      /* 0x104 */
+    s32 unk108;      /* 0x108 */
+    u8 _pad10C[2];   /* 0x10C - 0x10D */
+    u16 unk10E;      /* 0x10E */
+    u16 unk110;      /* 0x110 */
+    u8 _pad112[6];   /* 0x112 - 0x117 */
 } AkaoChannelState;  /* total = 0x118 */
 
 /**
