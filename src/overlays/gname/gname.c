c1452802ce9490a1706d69ef5445ab9941b74469
celophi <csharris@protonmail.com>
2026-06-06 20:17:35 -0700

    Fix symbols

--- src/overlays/gname/gname.c
+++ src/overlays/gname/gname.c
@@ -1019,7 +1019,7 @@ u_long* emit_cursor_glyph(u_long* prim, u_long* ot, s16 x, s16 y)
     SPRT* sprt = (SPRT*)prim;
     
     SET_BGR0_PACKED(sprt, GPU_TINT_NEUTRAL);
-    setSprt(sprt);
+    setSprt(sprt);a
     setXY0(sprt, x, y);
     
     setUV0(sprt, g_glyph_table[NAME_CURSOR_GLYPH_COUNT].u,
