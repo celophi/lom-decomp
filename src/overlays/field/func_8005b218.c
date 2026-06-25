5f8a6ba47857f2168621565dbc25f97a63100d41
celophi <csharris@protonmail.com>
2026-06-25 14:38:50 -0700

    Add func_8005B288

--- src/overlays/field/func_8005b218.c
+++ src/overlays/field/func_8005b218.c
@@ -5,7 +5,7 @@ extern unsigned int D_801ED02C;
 /**
  * @brief Return non-zero if D_801ED02C is set.
  * @return 1 if D_801ED02C != 0, 0 otherwise.
- * @see decomp.me TODO
+ * @see decomp.me (100%) TODO
  */
 s32 func_8005B218(void) {
     return D_801ED02C != 0;
