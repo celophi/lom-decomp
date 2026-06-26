c956134a9a6115ff223fa015c4dc8f7cc6b3422d
celophi <csharris@protonmail.com>
2026-06-26 13:15:01 -0700

    Add func

--- tools/permuter-scripts/compile.sh
+++ tools/permuter-scripts/compile.sh
@@ -8,7 +8,7 @@ TMPASM=$(mktemp --suffix=.s)
 cd /staging
 
 # Stage 1: Compile to assembly
-/opt/psx-gcc/gcc -B/opt/psx-gcc/ -O2 -G4 -gcoff -fsigned-char -Iinclude -S "$INPUT" -o "$TMPASM"
+/opt/psx-gcc-2.8.0/gcc -B/opt/psx-gcc-2.8.0/ -O2 -G4 -msoft-float -gcoff -fsigned-char -Iinclude -S "$INPUT" -o "$TMPASM"
 
 # Stage 2: Assemble with maspsx pipeline (like your build does)
 cat "$TMPASM" | python3 tools/maspsx/maspsx.py --run-assembler -Iinclude -no-pad-sections --aspsx-version=2.77 --expand-div -o "$OUTPUT"
