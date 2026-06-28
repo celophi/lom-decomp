dec0e51fb67b5b15b1332956869b25a6cace34e2
celophi <csharris@protonmail.com>
2026-06-28 09:00:50 -0700

    Add func

--- src/decomp9.c
+++ src/decomp9.c
@@ -98,3 +98,13 @@ void func_8002434C(s32 arg0, u32 arg1)
     arg0 = arg0 << 4;
     *(s16*)(ptr + arg0) = (s16)(arg1 >> 3);
 }
+
+/**
+ * decomp.me (100%) https://decomp.me/scratch/UI7qr
+ */
+void func_80024368(s32 arg0, u32 arg1)
+{
+    s32 ptr = (s32)0x1F801C0E;
+    arg0 = arg0 << 4;
+    *(s16*)(ptr + arg0) = (s16)(arg1 >> 3);
+}
