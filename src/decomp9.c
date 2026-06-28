85341058dd607f452478b6586450e0769b6ef391
celophi <csharris@protonmail.com>
2026-06-27 22:27:52 -0700

    Add func in decomp9 (#501)

---
+++ src/decomp9.c
@@ -0,0 +1,10 @@
+#include "common.h"
+
+/**
+ * decomp.me (100%) https://decomp.me/scratch/lKkom
+ */
+void func_80024260(u32 arg0)
+{
+    *(u16*)0x1F801D88 = arg0;
+    *(u16*)0x1F801D8A = arg0 >> 0x10;
+}
