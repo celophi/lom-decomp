7b5521cb353d86accf727bd28d56a67b57827df1
celophi <csharris@protonmail.com>
2026-06-30 15:23:48 -0700

    Update match func_80145278 (#511)

--- src/overlays/menu/menu.c
+++ src/overlays/menu/menu.c
@@ -3435,40 +3435,57 @@ void func_8014519C(void)
  *        bits 13:0  -- (i * 0x10) & 0x3FFF (slot identity / stride field),
  *        bits 22:14 -- previous circular index (wraps: entry 0's prev = arg0 - 1),
  *        bits 30:23 -- next circular index (wraps: last entry's next = 0).
- * @see decomp.me TODO
+ * @see decomp.me (100%) https://decomp.me/scratch/x87Jm
  */
-void func_80145278(s32 arg0)
-{
-    s32* temp_t0;
+void func_80145278(s32 arg0) {
     s32 temp_a1;
     s32 temp_a2;
     s32 temp_a3;
+    s32 temp_v1;
     s32 var_a2;
     s32 var_t1;
     s32 var_v1;
+    s32* temp_t0;
 
+    s32 tmp;
+    s32 tmp2;
+    s32 tmp3;
+    
     var_t1 = 0;
-    if (arg0 > 0)
-    {
-        do
-        {
-            temp_t0 = (s32*)((var_t1 * 4) + (u32)D_801690B8);
+    if (arg0 > 0) {
+        do {
+            temp_t0 = (var_t1) + D_801690B8;
+            
+            tmp = *temp_t0;
             var_a2 = var_t1 - 1;
-            *temp_t0 = (*temp_t0 & ~0x3FFF) | ((var_t1 * 0x10) & 0x3FFF);
-            if (var_a2 < 0)
-            {
+
+            
+            temp_v1 = (tmp & ~0x3FFF);
+
+            tmp2 = (var_t1 * 0x10);
+            tmp2 = tmp2 & 0x3FFF;
+            
+            temp_v1 = temp_v1  | tmp2;
+            *temp_t0 = temp_v1;
+            
+            if (var_a2 < 0) {
                 var_a2 = arg0 - 1;
             }
-            temp_a1 = (*temp_t0 & 0xFF803FFF) | ((var_a2 & 0x1FF) << 0xE);
-            *temp_t0 = temp_a1;
+
+            tmp3 = (temp_v1 & 0xFF803FFF);
+            
+            
+            tmp3 = tmp3 | ((var_a2 & 0x1FF) << 0xE);
+
+            
+            *temp_t0 = tmp3;
             temp_a2 = var_t1 + 1;
             temp_a3 = temp_a2 < arg0;
             var_v1 = 0;
-            if (temp_a3 != 0)
-            {
+            if (temp_a3 != 0) {
                 var_v1 = temp_a2;
             }
-            *temp_t0 = (temp_a1 & 0x7FFFFF) | (var_v1 << 0x17);
+            *temp_t0 = (tmp3 & 0x7FFFFF) | (var_v1 << 0x17);
             var_t1 = temp_a2;
         } while (temp_a3 != 0);
     }
