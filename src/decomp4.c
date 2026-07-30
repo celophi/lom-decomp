0cb53879fa29e5105ae7fd8898ee11aa5c68e9e2
celophi <csharris@protonmail.com>
2026-07-30 08:26:38 -0700

    Add function

--- src/decomp4.c
+++ src/decomp4.c
@@ -3122,3 +3122,37 @@ void func_8002C7D0(s_8002C79C* arg0)
     arg0->unk100 = new_unk100;
     arg0->unk10E = new_unk10E;
 }
+
+typedef struct {
+    u8* unk0;
+    u8 pad1[0x34 - 0x4];
+    s32 unk34;
+    u8 pad2[0x100 - 0x38];
+    s32 unk100;
+    u8 pad3[0x110 - 0x104];
+    u16 unk110;
+} s_8002C800;
+
+/**
+ * decomp.me (100%) https://decomp.me/scratch/Tun26
+ */
+void func_8002C800(s_8002C800* arg0)
+{
+    u8* ptr;
+    u32 byte_val;
+    u32 new_unk100;
+    u32 new_unk34;
+    u16 new_unk110;
+
+    ptr = arg0->unk0;
+    byte_val = *ptr;
+    arg0->unk0 = ptr + 1;
+
+    new_unk100 = arg0->unk100 | 0x2200;
+    new_unk34 = arg0->unk34 | 0x08000000;
+    new_unk110 = (arg0->unk110 & 0xE03F) | (byte_val << 6);
+
+    arg0->unk100 = new_unk100;
+    arg0->unk34 = new_unk34;
+    arg0->unk110 = new_unk110;
+}
