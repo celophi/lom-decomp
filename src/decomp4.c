f525eb47be09f617fd9812fec24f4b8a37edf258
celophi <csharris@protonmail.com>
2026-07-31 12:39:51 -0700

    Add function

--- src/decomp4.c
+++ src/decomp4.c
@@ -3604,3 +3604,30 @@ void func_8002CD88(AkaoChannelState* arg0)
         *(s16*)((u8*)arg0 + 0xD4) = 0x101;
     }
 }
+
+/**
+ * decomp.me (100%) https://decomp.me/scratch/j1qFh
+ */
+void func_8002CDB8(void* arg0, s32 arg1)
+{
+    u8* temp_v0;
+    s32 temp_v1;
+    s16 var_v0;
+
+    temp_v0 = *(u8**)arg0;
+    temp_v1 = *temp_v0;
+
+    *(u8**)arg0 = temp_v0 + 1;
+
+    if (temp_v1 != 0)
+    {
+        var_v0 = temp_v1 + 1;
+    }
+    else
+    {
+        var_v0 = 0x101;
+    }
+
+    *(s16*)((u8*)arg0 + 0xD6) = var_v0;
+    akao_seq_op_enable_pitch_modulation(arg0, arg1);
+}
