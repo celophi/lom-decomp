a966d21be56601593e57e56db244c1547fb31932
celophi <csharris@protonmail.com>
2026-08-17 20:53:44 -0700

    Fix one hack for another

--- src/akao_driver_init_state.c
+++ src/akao_driver_init_state.c
@@ -69,7 +69,7 @@ typedef struct { u8 pad40[0x40]; u32 f40; } A0S40;
  */
 void akao_driver_init_state(void)
 {
-    u32 a1var = 0x1F801DAA;
+    u32 hw = 0x1F801DAA;
     u32 t0 = 0x18;
     u8** new_var4;
     u32* new_var3;
@@ -77,12 +77,21 @@ void akao_driver_init_state(void)
     u32* new_var;
     int new_var7;
     int new_var5;
-    u8* a0 = &g_akao_seq_master_state;
-    u8* a2 = &g_akao_seq_channels;
+    u8* state;
+    u8* a0;
+    u8* a2;
     int new_var6;
     u8* new_var9;
+    u32 loop_one;
+    u32 tail_value;
     u32 a3;
-    u8** escape_a0 = &a0;
+
+
+    state = &g_akao_seq_master_state;
+    state = (u8 *)((u32)state ^ 1);
+    state = (u8 *)((u32)state ^ 1);
+
+    a2 = &g_akao_seq_channels;
 
     new_var3 = (u32*)off(D_8003EC30, 4);
     *new_var3 = 0;
@@ -98,24 +107,24 @@ void akao_driver_init_state(void)
     new_var = (u32*)off(D_8004F830, 0x00);
 
     *((u32*)off(g_akao_sfx_control, 0x00)) = 0;
-    *((u32*)off(a0, 0x04)) = 0;
-    *((u32*)off(a0, 0x08)) = 0;
-    *((u16*)off(a0, 0x5E)) = 0;
+    *((u32*)off(state, 0x04)) = 0;
+    *((u32*)off(state, 0x08)) = 0;
+    *((u16*)off(state, 0x5E)) = 0;
     *((u32*)off(g_akao_sfx_control, 0x10)) = 0;
-    *((u32*)off(a0, 0x1C)) = 0;
+    *((u32*)off(state, 0x1C)) = 0;
     *((u16*)off(D_8004C2D0, 0x5E)) = 0;
     *((u32*)off(D_8004C2D0, 0x04)) = 0;
-    *((u32*)off(a0, 0x50)) = 0x7F0000;
+    *((u32*)off(state, 0x50)) = 0x7F0000;
 
     D_8003EC58 = a2;
-    a2 = (u8*)((u32)a2 ^ 1);
-    a2 = (u8*)((u32)a2 ^ 1);
+    a2 = (u8 *)((u32)a2 ^ 1);
+    a2 = (u8 *)((u32)a2 ^ 1);
     a2 += 0x58;
-    g_akao_seq_channel0 = a0;
+    g_akao_seq_channel0 = state;
     g_akao_seq_channel1 = 0;
     g_akao_pending_channels = 0;
     g_akao_cdvol_tick = 0;
-    *((u16*)off(a0, 0x58)) = 0;
+    *((u16*)off(state, 0x58)) = 0;
     g_akao_cdvol_acc = 0x7FFF0000;
     g_akao_mastervol_fade_ticks = 0;
     g_akao_mastervol_acc = 0;
@@ -123,21 +132,21 @@ void akao_driver_init_state(void)
     g_akao_masterpan_acc = 0;
     g_akao_cdvol_fade_ticks = 0;
     *((u32*)off(g_akao_sfx_control, 0x1C)) = 0;
-    *((u32*)off(a0, 0x3C)) = 0;
+    *((u32*)off(state, 0x3C)) = 0;
     *((u32*)off(g_akao_sfx_control, 0x20)) = 0;
 
-    a3 = *(u16*)a1var;
-    ((A0S40*)a0)->f40 = 0;
+    a3 = *(u16*)hw;
+    ((A0S40*)state)->f40 = 0;
     *((s16*)0x1F801D80) = 0x3FFF;
     *((s16*)0x1F801D82) = 0x3FFF;
     *((s16*)0x1F801DB0) = 0x7FFF;
     *((s16*)0x1F801DB2) = 0x7FFF;
     *((u32*)off(g_akao_sfx_control, 0x24)) = 0;
-    *((u32*)off(a0, 0x44)) = 0;
-    *((u16*)off(a0, 0x68)) = 0;
-    *((u16*)off(a0, 0x66)) = 0;
-    *((u16*)off(a0, 0x64)) = 0;
-    *((u16*)off(a0, 0x6C)) = 0;
+    *((u32*)off(state, 0x44)) = 0;
+    *((u16*)off(state, 0x68)) = 0;
+    *((u16*)off(state, 0x66)) = 0;
+    *((u16*)off(state, 0x64)) = 0;
+    *((u16*)off(state, 0x6C)) = 0;
     *((u32*)off(g_akao_xa_tracker, 0x40)) = 0x7F00;
     *((u32*)off(g_akao_xa_tracker, 0x48)) = 0;
     g_akao_seq_pending_ticks = 0;
@@ -146,7 +155,7 @@ void akao_driver_init_state(void)
     *((u32*)off(D_8004F830, 0x08)) = 0;
     *((u32*)off(D_8004F830, 0x04)) = 0;
     *new_var = 0;
-    *(u16*)a1var = (a3 & 0xFFFA) | 1;
+    *(u16*)hw = (a3 & 0xFFFA) | 1;
     a3 = 0;
     do {
         a3++;
@@ -160,7 +169,7 @@ void akao_driver_init_state(void)
     } while ((a3 & 0xFFFF) < 0x20);
 
     a3 = 0xC;
-    a1var = 1;
+    loop_one = 1;
     new_var7 = 0x7F00;
     new_var5 = 0x8C;
     {
@@ -170,7 +179,7 @@ void akao_driver_init_state(void)
             a3++;
             *((u32*)(v1 + 0x34)) = 0;
             *((u32*)(v1 + 0xFC)) = tmp;
-            *((u16*)(v1 + 0x64)) = (u16)a1var;
+            *((u16*)(v1 + 0x64)) = (u16)loop_one;
             *((u32*)(v1 + 0x58)) = 0;
             *((u16*)(v1 + 0xE4)) = new_var7;
             *((u16*)(v1 + 0x8E)) = 0;
@@ -195,8 +204,8 @@ void akao_driver_init_state(void)
         *((u32*)off(v0_ptr, 0x0C)) = 0;
         *((u32*)off(v0_ptr, 0x08)) = 0;
         *((u32*)off(v0_ptr, 0x04)) = 0;
-        a1var = 0x03FFF000;
-        do { do { do { do { do { do { do { do { do { do { do { do { do { do { do { do { do { do { do { do { do { do { do { do { *((u32*)off(a0, 0x48)) = a1var; } while (0); } while (0); } while (0); } while (0); } while (0); } while (0); } while (0); } while (0); } while (0); } while (0); } while (0); } while (0); } while (0); } while (0); } while (0); } while (0); } while (0); } while (0); } while (0); } while (0); } while (0); } while (0); } while (0); } while (0);
+        tail_value = 0x03FFF000;
+        *((u32*)off(a0, 0x48)) = tail_value;
         *((u32*)off(a0, 0x4C)) = 0;
         *((u16*)off(a0, 0x5A)) = 0;
         new_var9 = v1_ptr;
