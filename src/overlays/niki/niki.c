b476c76c7d8e22b123e0f01047fb2a54743800d7
celophi <csharris@protonmail.com>
2026-08-22 10:44:18 -0700

    Add NIKI function

--- src/overlays/niki/niki.c
+++ src/overlays/niki/niki.c
@@ -18,6 +18,8 @@ extern s32 D_80164B78;
 extern s32 D_80164B84;
 extern s32 D_80164B8C;
 extern s32 D_80164B90;
+extern s32 D_8012271C, D_80164ADC, D_80164AE0, D_80164AEC;
+extern s32 D_80164B7C, D_80164B80, D_80164B88;
 
 void func_8014011C(s32 arg0, s32 arg1)
 {
@@ -63,4 +65,126 @@ s32 func_801401F0(s32 arg0)
     func_80063194();
     D_80164B8C ^= 1;
     return 0;
+}
+
+
+void func_80140D4C(); void func_801413FC(); void func_801414A8();
+void func_80141584(); void func_80141660(); void func_801433BC();
+void func_80141E84();
+
+typedef struct
+{
+    union
+    {
+        u32 word;
+        struct
+        {
+            u32 state : 3;
+            u32 unk0_3 : 4;
+            u32 x : 9;
+            u32 unk0_16 : 8;
+        } f;
+    } attr;
+    u32 unk4_0 : 1;
+    u32 y : 8;
+    u32 unk4_9 : 23;
+    void* draw_handler;
+} NikiElement;
+
+extern NikiElement D_80164B10;
+
+NikiElement *func_80141EC4();
+#define SET_ELEM_CODE(e, c) ((e)->attr.word = ((e)->attr.word & 0x00FFFFFF) | ((u32)(c) << 24))
+
+void func_8014027C(void)
+{
+    NikiElement *p;
+    D_80164B88 = 0;
+    D_80164AEC = 0;
+    D_80164AE0 = 0;
+    D_80164B7C = 0;
+    D_80164B84 = 0;
+    D_80164ADC = D_8012271C + 0xCE0;
+    if (0) func_80141E84(0,0,0,0,0);
+    func_80141E84();
+    D_80164B80 = 0;
+    if (D_80164AE8 != 0)
+    {
+        D_80164B10.attr.f.state = 1;
+        p = func_80141EC4();
+        p->draw_handler = (void *)func_801433BC;
+        p->attr.f.unk0_3 = 1;
+        p->attr.f.x = 0x10;
+        p->attr.f.unk0_16 = 0x61;
+        p->unk4_0 = 1;
+        p->y = 0x2C;
+        SET_ELEM_CODE(p, 0x20);
+
+        p = func_80141EC4();
+        p->draw_handler = (void *)func_801414A8;
+        p->attr.f.unk0_3 = 1;
+        p->attr.f.x = 0x18;
+        p->attr.f.unk0_16 = 0x4D;
+        p->unk4_0 = 0;
+        p->y = 0x10;
+        SET_ELEM_CODE(p, 0x80);
+
+        p = func_80141EC4();
+        p->draw_handler = (void *)func_80141584;
+        p->attr.f.unk0_3 = 1;
+        p->attr.f.x = 0xA0;
+        p->attr.f.unk0_16 = 0x4D;
+        p->unk4_0 = 0;
+        p->y = 0x10;
+        SET_ELEM_CODE(p, 0x80);
+        D_80164B10.attr.f.state = 0;
+        return;
+    }
+
+    D_80164B10.attr.f.state = 1;
+    p = func_80141EC4();
+    p->draw_handler = (void *)func_80140D4C;
+    p->attr.f.unk0_3 = 1;
+    p->attr.f.x = 0x1C;
+    p->attr.f.unk0_16 = 0x32;
+    p->unk4_0 = 1;
+    p->y = 0x58;
+    SET_ELEM_CODE(p, 8);
+
+    p = func_80141EC4();
+    p->draw_handler = (void *)func_801413FC;
+    p->attr.f.unk0_3 = 1;
+    p->attr.f.x = 0x24;
+    p->attr.f.unk0_16 = 0x0A;
+    p->unk4_0 = 0;
+    p->y = 0x10;
+    SET_ELEM_CODE(p, 0xF0);
+
+    p = func_80141EC4();
+    p->draw_handler = (void *)func_801414A8;
+    p->attr.f.unk0_3 = 1;
+    p->attr.f.x = 0x18;
+    p->attr.f.unk0_16 = 0x1E;
+    p->unk4_0 = 0;
+    p->y = 0x10;
+    SET_ELEM_CODE(p, 0x80);
+
+    p = func_80141EC4();
+    p->draw_handler = (void *)func_80141584;
+    p->attr.f.unk0_3 = 1;
+    p->attr.f.x = 0xA0;
+    p->attr.f.unk0_16 = 0x1E;
+    p->unk4_0 = 0;
+    p->y = 0x10;
+    SET_ELEM_CODE(p, 0x80);
+
+    p = func_80141EC4();
+    p->draw_handler = (void *)func_80141660;
+    p->attr.f.unk0_3 = 1;
+    p->attr.f.x = 0x1E;
+    p->attr.f.unk0_16 = 0x8E;
+    p->unk4_0 = 1;
+    p->y = 0x34;
+    SET_ELEM_CODE(p, 4);
+    D_80164B10.attr.f.state = 0;
 }
