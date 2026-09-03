3a47ca32483d6e8c32783691edd4c2d6270d3c0c
celophi <csharris@protonmail.com>
2026-09-03 07:34:03 -0700

    Add FIELD match

--- mk/overlay-registry.mk
+++ mk/overlay-registry.mk
@@ -426,6 +426,8 @@ overlay_field_gcc_272_cdk_g0_srcs := \
 	src/overlays/field/func_800CA568.c \
 	src/overlays/field/func_800CB680.c \
 	src/overlays/field/unk2_b_split098.c \
+	src/overlays/field/func_800CBC0C.c \
+	src/overlays/field/unk2_b_split098_tail.c \
 	src/overlays/field/func_800CBEC4.c \
 	src/overlays/field/field235.c \
 	src/overlays/field/func_800A8E28.c \
