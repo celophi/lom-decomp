9e91e1ca2184a7b8cb04e972363d7ac31c4bdb57
Christopher Harris <csharris@protonmail.com>
2026-09-20 13:57:24 -0700

    Add header

---
+++ include/field_ability_progression.h
@@ -0,0 +1,29 @@
+#ifndef FIELD_ABILITY_PROGRESSION_H
+#define FIELD_ABILITY_PROGRESSION_H
+
+#include "common.h"
+
+#define FIELD_ABILITY_UNLOCK_RULE_COUNT 18
+#define FIELD_ABILITY_PREREQUISITE_NONE 0xFF
+
+/** @brief An ability and the proficiency required to satisfy an unlock rule. */
+typedef struct
+{
+    u8 ability;
+    u8 proficiency;
+} FieldAbilityPrerequisite;
+
+/** @brief Two ability prerequisites and the ability they unlock. */
+typedef struct
+{
+    FieldAbilityPrerequisite prerequisites[2];
+    u8 result;
+} FieldAbilityUnlockRule;
+
+extern FieldAbilityUnlockRule g_field_ability_unlock_rules[];
+extern u16 g_field_progression_unlocks[];
+extern u16 g_field_progression_unlock_count;
+
+void field_advance_ability_progression(void);
+
+#endif
