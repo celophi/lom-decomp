649a0c1e8480e0c98be7b96af6b152dc5d68f236
Christopher Harris <csharris@protonmail.com>
2026-09-20 20:29:07 -0700

    Add func_80054A2C

--- mk/overlay-registry.mk
+++ mk/overlay-registry.mk
@@ -346,6 +346,7 @@ overlay_title_gcc_272_cdk_g0_srcs := \
 
 OVERLAYS += wmap
 overlay_wmap_gcc_280_g0_srcs := \
+	src/overlays/wmap/func_80054A2C.c \
 	src/overlays/wmap/func_800551A8.c \
 	src/overlays/wmap/func_8005536C.c \
 	src/overlays/wmap/func_8005556C.c
