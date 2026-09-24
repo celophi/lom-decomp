2facb9ecbc3961ef848f24063d7d2c8c4a2e631a
Christopher Harris <csharris@protonmail.com>
2026-09-24 13:01:23 -0700

    Add matches in the wmap_effect-primitives

--- include/wmap_effect_primitives.h
+++ include/wmap_effect_primitives.h
@@ -5,12 +5,14 @@
 #include "sdk/libgte.h"
 
 void func_8006A2FC(void* arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4_value, s32 arg5_value, u32 arg6, void* arg7);
-void func_8006A9C4(s32 arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5, s32 arg6, s32 arg7, s32 arg8, u16 arg9, s32 arg10, s32 arg11);
+void func_8006A9C4(s32 actor_address, s32 resource_address, s32 first, s32 end, s32 scale,
+                   s32 velocity_min, s32 velocity_range, s32 lifetime_min, s32 lifetime_range, s32 initial_z,
+                   s32 frame, s32 spawn_interval);
 void func_8006ADD0(VECTOR* translation, SVECTOR* rotation);
 void func_8006AEE0(void);
-void func_8006AFAC(s32 arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5, s32 arg6, u16 arg7);
-void func_8006B328(s32 arg0, s32 arg1, s32 arg2, s16 arg3, s32 arg4, s32 arg5, u16 arg6, s32 arg7, s32 arg8, s32 arg9, s32 arg10, s32 arg11, s32 arg12,
-                   u16 arg13, u16 arg14, u16 arg15, s32 arg16);
+void func_8006AFAC(s32 first, s32 end, s32 frame, s32 depth,
+                   s32 velocity_base, s32 velocity_divisor, s32 lifetime_base, s32 sequence);
+void func_8006B328(s32 first, s32 end, s32 spawn_interval, s32 scale_override, s32 velocity_min, s32 velocity_range, s32 initial_z, s32 frame, s32 x_min, s32 x_range, s32 y_min, s32 y_range, s32 fade_z, s32 initial_scale_target, s32 initial_scale, s32 scale_step, s32 group);
 void func_8006B6EC(s32 first, s32 end, s32 frame, s32 z_step, s32 depth);
 void func_8006B998(s32 first, s32 end, void* point_data, s32 frame, s32 depth);
 void func_8006BC44(s32 arg0, s32 arg1, void* arg2, s32 arg3);
