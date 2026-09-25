6c4a49eb71f742e2ec8e4abbc4c021736e11e672
Christopher Harris <csharris@protonmail.com>
2026-09-24 23:11:01 -0700

    Add WMAP travel sequences match

--- src/overlays/wmap/wmap_travel_sequences.c
+++ src/overlays/wmap/wmap_travel_sequences.c
@@ -276,209 +276,141 @@ extern WmapMotion D_801AFBD0;
 
 s32 func_80099B50(s32 arg3)
 {
-/* Partial WMAP decompilation: 82.702440% (gcc280_g0). */
+    extern s32 D_800D9164;
+    extern s32 D_800DCED8;
+    extern s32 D_800DCEE4;
+    extern s32 D_800DCEF8;
+    extern s32 D_800DCF00;
+    extern s32 D_801398D0;
+    extern s32 D_80182D68;
+    extern s32 D_80182D78;
+    extern s32 D_8019D240;
+    s32 dx;
+    s32 dy;
+    s32 direction_y;
+    s32 difference;
+    s32 sign;
+    s32 tail_dy;
+    s32 tail_magnitude_x;
+    s32 tail_magnitude_y;
 
-typedef s32 M2C_UNK;
-typedef s8 M2C_UNK8;
-typedef s16 M2C_UNK16;
-typedef s32 M2C_UNK32;
-#define M2C_FIELD(expr, type_ptr, offset) (*(type_ptr)((s8 *)(expr) + (offset)))
-#define M2C_UNALIGNED32(expr) (expr)
-#define M2C_BITWISE(type, expr) ((type)(expr))
-
-extern s32 D_800D9164;
-extern s32 D_800DCED8;
-extern s32 D_800DCEE4;
-extern s32 D_800DCEF8;
-extern s32 D_800DCF00;
-extern s32 D_801398D0;
-extern s32 D_80182D68;
-extern s32 D_80182D78;
-extern s32 D_8019D240;
-extern u8 func_80099E84__for_func_80099B50 __asm__("func_80099E84");
-extern u8 func_80099F98__for_func_80099B50 __asm__("func_80099F98");
-
-    void *var_a0_3;
-    s32 *var_a1;
-    s32 temp_a2;
-    s32 temp_a3;
-    s32 temp_t1;
-    s32 temp_v1;
-    s32 temp_v1_2;
-    s32 var_a0;
-    s32 var_a0_2;
-    s32 var_a2;
-    s32 var_a3;
-    s32 var_v0;
-    s32 var_v0_2;
-    s32 var_v0_3;
-    s32 var_v0_4;
-    s32 var_v0_5;
-    s32 var_v0_6;
-    s32 var_v0_7;
-    s32 var_v1;
-
-    var_a3 = arg3;
     D_8019D240 = -1;
     D_800D9164 = 0;
-    if (D_800DCEF8 == D_800DCED8)
+    if ((D_800DCEF8 == D_800DCED8 && D_800DCF00 != D_800DCEE4) ||
+    (D_800DCEF8 != D_800DCED8 && D_800DCF00 == D_800DCEE4))
     {
-        if (D_800DCF00 == D_800DCEE4)
+        dx = D_800DCED8 - D_800DCEF8;
+        dy = D_800DCEE4 - D_800DCF00;
+        if (dx != 0)
         {
-            goto block_12;
+            dx = dx / __builtin_abs(dx) + 1;
         }
-        goto block_4;
-    }
-    if (D_800DCF00 == D_800DCEE4)
-    {
-block_4:
-        var_a1 = &D_800DCF00;
-        temp_a2 = D_800DCED8 - D_800DCEF8;
-        var_a0 = D_800DCEE4 - D_800DCF00;
-        if (temp_a2 != 0)
+        else
         {
-            var_v0 = temp_a2;
-            if (temp_a2 < 0)
+            dx = 1;
+        }
+        if (dy != 0)
+        {
+            dy = dy / __builtin_abs(dy) + 1;
+        }
+        else
+        {
+            dy = 1;
+        }
+        D_8019D240 = dx + dy * 3;
+    }
+    else
+    {
+        direction_y = D_800DCED8 - D_800DCEF8;
+        dy = D_800DCEE4 - D_800DCF00;
+        if (__builtin_abs(direction_y) == __builtin_abs(dy))
+        {
+            dx = direction_y;
+            if (dx != 0)
             {
-                var_v0 = -var_v0;
+                dx = dx / __builtin_abs(dx) + 1;
             }
-            var_a2 = (temp_a2 / var_v0) + 1;
-        }
-        else
-        {
-            var_a2 = 1;
-        }
-        if (var_a0 == 0)
-        {
-            var_a0_2 = 1;
-        }
-        else
-        {
-            goto block_21;
-        }
-        goto block_25;
-    }
-block_12:
-    var_a2 = D_800DCED8;
-    temp_v1 = var_a2 - D_800DCEF8;
-    var_a0 = D_800DCEE4 - D_800DCF00;
-    var_a1 = (s32 *) temp_v1;
-    if (temp_v1 < 0)
-    {
-        var_a1 = (s32 *) -(s32) var_a1;
-    }
-    var_v0_2 = var_a0;
-    if (var_a0 < 0)
-    {
-        var_v0_2 = -var_v0_2;
-    }
-    if (var_a1 == var_v0_2)
-    {
-        if (temp_v1 != 0)
-        {
-            var_a2 = (temp_v1 / (s32) var_a1) + 1;
-        }
-        else
-        {
-            var_a2 = 1;
-        }
-        if (var_a0 != 0)
-        {
-block_21:
-            var_v0_3 = var_a0;
-            if (var_a0 < 0)
+            else
             {
-                var_v0_3 = -var_v0_3;
+                dx = 1;
             }
-            var_a0_2 = (var_a0 / var_v0_3) + 1;
+            if (dy != 0)
+            {
+                dy = dy / __builtin_abs(dy) + 1;
+            }
+            else
+            {
+                dy = 1;
+            }
+            D_8019D240 = dx + dy * 3;
         }
-        else
-        {
-            var_a0_2 = 1;
-        }
-block_25:
-        D_8019D240 = var_a2 + (var_a0_2 * 3);
     }
     if (D_8019D240 != -1)
     {
         D_801398D0 = 2;
-        var_a0_3 = &func_80099E84__for_func_80099B50;
+        func_8006CBD8(func_80099E84);
     }
     else
     {
-        var_a2 = D_800DCED8 - D_800DCEF8;
-        temp_t1 = D_800DCEE4 - D_800DCF00;
-        var_v1 = var_a2;
-        if (var_a2 < 0)
+        dx = D_800DCED8 - D_800DCEF8;
+        tail_dy = D_800DCEE4 - D_800DCF00;
+        tail_magnitude_x = __builtin_abs(dx);
+        tail_magnitude_y = __builtin_abs(tail_dy);
+        difference = tail_magnitude_x - tail_magnitude_y;
+        if (difference > 0)
         {
-            var_v1 = -var_v1;
-        }
-        var_v0_4 = temp_t1;
-        if (temp_t1 < 0)
-        {
-            var_v0_4 = -var_v0_4;
-        }
-        temp_a3 = var_v1 - var_v0_4;
-        if (temp_a3 > 0)
-        {
-            var_v0_5 = 0;
-            if (var_a2 != 0)
+            sign = 0;
+            if (dx != 0)
             {
-                var_v0_5 = -1;
-                if (var_a2 > 0)
+                sign = -1;
+                if (dx > 0)
                 {
-                    var_v0_5 = 1;
+                    sign = 1;
                 }
             }
-            var_a1 = &D_80182D68;
+            difference = difference * sign;
             D_80182D78 = 0;
-            var_a3 = temp_a3 * var_v0_5;
-            D_80182D68 = var_a3 * 0x30;
-            D_800DCEF8 += var_a3;
-            if (var_a2 > 0)
+            D_80182D68 = difference * 48;
+            D_800DCEF8 += difference;
+            if (dx > 0)
             {
-                var_v0_6 = 5;
+                D_8019D240 = 5;
             }
             else
             {
-                var_v0_6 = 3;
+                D_8019D240 = 3;
             }
         }
         else
         {
-            temp_v1_2 = D_800DCF00 - D_800DCEE4;
-            var_v0_7 = 0;
-            if (temp_v1_2 != 0)
+            direction_y = D_800DCF00 - D_800DCEE4;
+            sign = 0;
+            if (direction_y != 0)
             {
-                var_v0_7 = -1;
-                if (temp_v1_2 > 0)
+                sign = -1;
+                dx = direction_y > 0;
+                if (dx)
                 {
-                    var_v0_7 = 1;
+                    sign = 1;
                 }
             }
-            var_a1 = &D_80182D78;
+            difference = difference * sign;
             D_80182D68 = 0;
-            var_a3 = temp_a3 * var_v0_7;
-            D_80182D78 = var_a3 * 0x30;
-            D_800DCF00 += var_a3;
-            if (temp_t1 > 0)
+            D_80182D78 = difference * 48;
+            D_800DCF00 += difference;
+            if (tail_dy > 0)
             {
-                var_v0_6 = 7;
+                D_8019D240 = 7;
             }
             else
             {
-                var_v0_6 = 1;
+                D_8019D240 = 1;
             }
         }
-        D_8019D240 = var_v0_6;
-        var_a0_3 = &func_80099F98__for_func_80099B50;
+        func_8006CBD8(func_80099F98);
     }
-    func_8006CBD8(var_a0_3);
     return 0;
 }
-#undef M2C_FIELD
-#undef M2C_UNALIGNED32
-#undef M2C_BITWISE
 
 /**
  * @brief Start map movement after the actor faces its route, then wait for completion.
